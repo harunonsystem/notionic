d76c995061295009558d0851ae786b5b012919bb
harunon <harunon@harunonsystem.com>
2024-12-22 17:48:48 +0900

    refactor: filterPublishedPosts

--- lib/notion/filterPublishedPosts.ts
+++ lib/notion/filterPublishedPosts.ts
@@ -23,22 +23,23 @@ export default function filterPublishedPosts({
 }: filterPublishedPostsProps) {
   if (!posts || !posts.length) return []
 
-  
-  return posts
-    .filter(post => {
-      const type = post?.type?.[0]
-      
-      if (onlyNewsletter && type !== 'Newsletter') return false
-      if (onlyNotes && type !== 'Notes') return false  
-      if (onlyPost && type !== 'Post') return false
-      if (onlyWeekly && type !== 'Weekly') return false
-      if (onlyHidden ? type !== 'Hidden' : type === 'Hidden') return false
+  return posts.filter((post) => {
+    const type = post?.type?.[0]
 
-      return (
-        post.title &&
-        post.slug &&
-        post?.status?.[0] === 'Published' &&
-        post.date <= new Date()
-      )
-    })
+    if (
+      (onlyNewsletter && type !== 'Newsletter') ||
+      (onlyNotes && type !== 'Notes') ||
+      (onlyPost && type !== 'Post') ||
+      (onlyWeekly && type !== 'Weekly') ||
+      (onlyHidden ? type !== 'Hidden' : type === 'Hidden')
+    )
+      return false
+
+    return (
+      post.title &&
+      post.slug &&
+      post.status?.[0] === 'Published' &&
+      post.date <= new Date()
+    )
+  })
 }
