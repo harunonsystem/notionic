f2b64fa8c91574375a2988d8aef83ee0d6d06759
harunon <harunonsystem@gmail.com>
2025-10-17 14:08:30 +0900

    css fix

--- components/Post/ShareButton.tsx
+++ components/Post/ShareButton.tsx
@@ -33,7 +33,7 @@ const ShareButton = ({ title }: ShareButtonProps) => {
           <button
             type='button'
             disabled
-            className='flex gap-1 bg-gray-400 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-400 text-sm rounded-lg px-4 py-2'
+            className='flex gap-1 bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
           >
             <LinkIcon className='flex flex-col justify-center items-center select-none cursor-pointer relative w-5 h-5' />
             {t.LAYOUT.COPY_TITLE_AND_URL_BUTTON_TEXT_COPIED}
@@ -42,7 +42,7 @@ const ShareButton = ({ title }: ShareButtonProps) => {
           <button
             type='button'
             onClick={() => copyTitleAndUrl(title, window.location.href)}
-            className='flex gap-1 bg-gray-400 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-400 text-sm rounded-lg px-4 py-2'
+            className='flex gap-1 bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
           >
             <LinkIcon className='flex flex-col justify-center items-center select-none cursor-pointer relative w-5 h-5' />
             {t.LAYOUT.COPY_TITLE_AND_URL_BUTTON_TEXT}
@@ -51,7 +51,7 @@ const ShareButton = ({ title }: ShareButtonProps) => {
         <button
           type='button'
           onClick={() => shareOnTwitter(title, window.location.href)}
-          className='flex gap-1 bg-gray-400 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-400 text-sm rounded-lg px-4 py-2'
+          className='flex gap-1 bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
         >
           <svg
             className='w-5 h-5'
